c8bfdb3acde8dfc32bde0e264b912ca01d065495
Caio Alves Fiorotti <caiofiorotti@gmail.com>
2024-05-07 13:31:03 -0300

    :clap: cdeleting shared memory funcs and comments

--- Assets/Scripts/Body Control/ControlArm.cs
+++ Assets/Scripts/Body Control/ControlArm.cs
@@ -16,10 +16,6 @@ public class MoveArm : MonoBehaviour {
     public Transform elbowJoint; // The arm that will be controlled
     public Transform graphBar; // The graphic bar that will ilustrate the albow angle
 
-    /* Debug */
-    // private float elbowAngle = 90.0f; // Angle of the elbow
-    // private bool change = true;
-
     private string csvFilePath = "Data/biofeedback-graph-bar-" + DateTime.Now.ToString("dd-MM-yyyy_HH-mm-ss") + ".csv";
     private string csvSeparator = ";";
     private string csvContent;
@@ -41,11 +37,6 @@ public class MoveArm : MonoBehaviour {
 
     private float TIME_OF_GAME = 5f; // Time of the game
 
-    const string sharedMemoryName = "SharedMemoryMap";
-    const int bufferSize = 2^16;
-    private MemoryMappedFile memoryMappedFile;
-    private MemoryMappedViewStream memoryMappedViewStream;
-
 
     // Start is called before the first frame update
     void Start() {
@@ -57,34 +48,11 @@ public class MoveArm : MonoBehaviour {
         /* Set the time to 0 */
         time = 0.0f;
         targetAngleTime = 0.0f;
-
-        // CreateSharedMemory();
     }
+    
 
     // Update is called once per frame
     void Update() {
-        // System.Random random = new System.Random();
-        // string[] values = new string[6];
-        // values[0] = UnityEngine.Random.Range(300, 4000).ToString();
-        // values[1] = UnityEngine.Random.Range(150, 2500).ToString();
-        // values[2] = random.NextDouble().ToString();
-        // values[3] = random.NextDouble().ToString();
-        // // values[4] = UnityEngine.Random.Range(0, 90).ToString();
-        // /* Debug */
-        // values[4] = elbowAngle.ToString();
-        // if (!change) {
-        //     elbowAngle += 1; // Limita o ângulo mínimo
-        //     if (elbowAngle == 90) change = true;
-        // } else {
-        //     elbowAngle -= 1; // Limita o ângulo máximo
-        //     if (elbowAngle == 0) change = false;
-        // }
-        // values[5] = UnityEngine.Random.Range(0, 7).ToString();
-
-        // /* Debug */
-        // string serialData = values[0] + "," + values[1] + "," + values[2] + "," + values[3] + "," + values[4] + "," + values[5]; // Create a string with the values
-        // Debug.Log(serialData);
-
         if (serialPort.IsOpen) {
             string serialData = serialPort.ReadLine(); // Reads a line of data from the serial port
             string[] values = serialData.Split(','); // Divide values separated by comma
@@ -95,10 +63,9 @@ public class MoveArm : MonoBehaviour {
             uf = uf / 100; // Convert
             ue = ue / 100; // Convert
             
-            Debug.Log(fileCount + " uf: " + uf + " ue: " + ue);
-            // Debug.Log(elbowAngle);
+            Debug.Log(elbowAngle);
 
-            saveUData(uf, ue); // Save the data in a csv file
+            saveUData(uf, ue); // Save the data in a csv file to read in python
 
             if (startSavingData) {
                 saveData(elbowAngle, time); // Save the data in a csv file
@@ -124,16 +91,19 @@ public class MoveArm : MonoBehaviour {
         targetAngleTime += Time.deltaTime; 
     }
 
+
     void saveUData(float uf, float ue) {
         uDataContent = fileCount + uDataSeparator + uf.ToString() + uDataSeparator + ue + "\n"; // Create the csv content with the time
         File.AppendAllText(uDataFilePath, uDataContent); // Append the content to the csv file
         fileCount++;
     }
+    
 
     void saveData(float elbowAngle, float time) {
         csvContent = elbowAngle.ToString() + csvSeparator + time + "\n"; // Create the csv content with the time
         File.AppendAllText(csvFilePath, csvContent); // Append the content to the csv file
     }
+
 
     public void toggleStartSaveData() {
         startSavingData = !startSavingData;
@@ -147,51 +117,8 @@ public class MoveArm : MonoBehaviour {
         csvFilePath = "Data/biofeedback-graph-bar-" + DateTime.Now.ToString("dd-MM-yyyy_HH-mm-ss") + ".csv"; // Update the csv file path
     }
 
-    // Create shared memory
-    void CreateSharedMemory() {
-        try {
-            memoryMappedFile = MemoryMappedFile.CreateOrOpen(sharedMemoryName, bufferSize);
-            memoryMappedViewStream = memoryMappedFile.CreateViewStream();
-            Debug.Log("Shared memory created.");
-        }
-        catch (Exception e) {
-            Debug.LogError("Error creating shared memory: " + e.Message);
-        }
-    }
-
-    // Write data to shared memory
-    void writeToSharedMemory(string data) {
-        if (memoryMappedViewStream != null) {
-            try {
-                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(data);
-                memoryMappedViewStream.Write(buffer, 0, buffer.Length);
-                Debug.Log("Data written to shared memory: " + data);
-            }
-            catch (Exception e) {
-                Debug.LogError("Error writing to shared memory: " + e.Message);
-            }
-        }
-        else {
-            Debug.LogError("Shared memory not initialized.");
-        }
-    }
-
-    // Close shared memory
-    void CloseSharedMemory() {
-        if (memoryMappedViewStream != null) {
-            memoryMappedViewStream.Dispose();
-            memoryMappedViewStream = null;
-        }
-        if (memoryMappedFile != null) {
-            memoryMappedFile.Dispose();
-            memoryMappedFile = null;
-        }
-        Debug.Log("Shared memory closed.");
-    }
-
 
     void OnApplicationQuit() {
         if (serialPort.IsOpen) serialPort.Close();
-        // CloseSharedMemory();
     }
 }
